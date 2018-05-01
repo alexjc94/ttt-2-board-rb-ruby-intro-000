7c42bd883f8fea05eab576f97858012ea2191057
Alexandra Carroll <alexandrajc94@gmail.com>
2018-05-01 04:41:43 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,6 @@
 my_array=[board]
 board = [0,1,2,3,4,5,6,7,8]
-row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Position 1: Left
 row["0"]
 # Position 2: Middle
