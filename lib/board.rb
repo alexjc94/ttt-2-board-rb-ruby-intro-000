51cb07657ba78a97c547f2a6f422df56b8f7c57a
Alexandra Carroll <alexandrajc94@gmail.com>
2018-05-01 04:47:21 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -2,20 +2,20 @@ my_array=["board"]
 board = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
 row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Position 1: Left
-row["0"]
+row[0]
 # Position 2: Middle
-row["1"]
+row[1]
 # Position 3: Right
-row["2"]
+row[2]
 # Position 4: Left
-row["3"]
+row[3]
 # Position 5: Middle
-row["4"]
+row[4]
 # Position 6: Right
-row["5"]
+row[5]
 # Position 7: Left
-row["6"]
+row[6]
 # Position 8: Middle
-row["7"]
+row[7]
 # Position 9: Right
-row["8"]
+row[8]
